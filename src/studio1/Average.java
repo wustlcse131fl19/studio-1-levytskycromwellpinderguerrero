22b6ec4fda1e20661c27bbcfadfeb09fdcad749d
andrianalevytsky <andrianalevytsky@wustl.edu>
2019-08-29 15:28:39 -0500

    studio 1

---
+++ src/studio1/Average.java
@@ -0,0 +1,14 @@
+package studio1;
+
+import support.cse131.ArgsProcessor;
+
+public class Average {
+    public static void main(String[] args) {
+        ArgsProcessor ap = new ArgsProcessor(args);
+        int n1 = ap.nextInt("What is your first integer?");
+        int n2 = ap.nextInt("What is your second integer?");
+        double average = ((double) n1+ n2)/2;
+        
+        System.out.println("The average of your two integers is "+average);
+    } }
+
